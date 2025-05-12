2c6b5cdfd3df1f90ab3d80243784e3a677b2b757
Michael Karmazin <michael.karmazin@algoteq.com>
2025-05-12 12:35:30 +1000

    Use cache.putAsync instead of DataStreamer

--- src/main/java/org/example/DemoWriter.java
+++ src/main/java/org/example/DemoWriter.java
@@ -3,15 +3,10 @@ package org.example;
 import static org.example.Tools.getCacheConfig;
 
 import java.util.Base64;
-import java.util.Collection;
-import java.util.Map;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.IgniteDataStreamer;
-import org.apache.ignite.binary.BinaryObject;
 import org.apache.ignite.configuration.CacheConfiguration;
-import org.apache.ignite.stream.StreamReceiver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -32,36 +27,20 @@ public class DemoWriter {
   private void run() {
     try {
       CacheConfiguration<String, DataA> cacheConfig = getCacheConfig();
+      IgniteCache<String, DataA> cache = ignite.getOrCreateCache(cacheConfig);
 
-      try (IgniteDataStreamer<String, DataA> streamer = ignite.dataStreamer(
-        cacheConfig.getName()))
-      {
+      byte[] bytes = new byte[1024];
+      for (int i = 0; i < 10_000; i++) {
+        randomiseBytes(i, bytes);
+        String data = Base64.getEncoder().encodeToString(bytes);
 
-        streamer.allowOverwrite(true);
-        streamer.keepBinary(true);
-        streamer.skipStore(true);
-        streamer.autoFlushFrequency(1_000);
+        String key = "key-" + (i % 300);
+        DataA value = new DataA(data, i);
 
-        // Need to cast it to untyped - while the streamer itself is typed
-        // for <String, DataA>, it also has keepBinary(true), so the receiver
-        // actually gets <BinaryObject, BinaryObject> pairs:
-        streamer.receiver((StreamReceiver)DemoWriter::receive);
-
-        byte[] bytes = new byte[1024];
-        for (int i = 0; i < 10_000; i++) {
-          randomiseBytes(i, bytes);
-          String data = Base64.getEncoder().encodeToString(bytes);
-
-          String key = "key-" + (i % 300);
-          DataA value = new DataA(data, i);
-
-          streamer.addData(key, value);
-
-          //spinWait(1_000);
-        }
+        cache.putAsync(key, value);
       }
 
-      LOG.info("Finished writing & updating data.");
+      LOG.info("Finished writing & updating data WITH CACHE-PUT-ASYNC.");
     } catch (Throwable err) {
       LOG.error("Error", err);
     }
@@ -71,16 +50,4 @@ public class DemoWriter {
     for (int i = 0; i < bytes.length; i++)
       bytes[i] = (byte)(i ^ seed);
   }
-
-  private static void receive(IgniteCache<BinaryObject, BinaryObject> cache,
-    Collection<Map.Entry<BinaryObject, BinaryObject>> entries)
-  {
-    for (Map.Entry<BinaryObject, BinaryObject> e : entries) {
-      cache.invokeAsync(e.getKey(), (entry, args) -> {
-        entry.setValue(e.getValue());
-        return null;
-      });
-    }
-  }
-
 }
